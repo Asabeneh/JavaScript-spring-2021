0ea27aadf48532e26477fefe3f5d0ec46cc1827e
asabeneh <asabeneh@gmail.com>
2021-03-15 20:58:30 +0200

    week 1

---
+++ countries.js
@@ -0,0 +1,1 @@
+console.log('This file going to have list of countries')
---
+++ script.js
@@ -0,0 +1,25 @@
+
+// this is JavaScript single line comment
+// comments make your code more readable
+
+/*
+Comment
+might go
+in different 
+lines
+
+*/
+console.log('This is text I want to print')
+console.log('item 1', 'item 2', 'item 3', 'COVID-19')
+console.log()
+
+// Data types in Java: Number, String, Boolean, Null, Undefined
+
+console.log(10)
+console.log(2 + 3)
+console.log(2 - 3)
+console.log(2 * 3)
+console.log(13 % 5)
+console.log(3 * 3  * 3)
+console.log(3 ** 3)
+console.log(2 + 2 == 2 * 2)
